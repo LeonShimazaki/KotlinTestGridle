868388e606aa765c7f959c7bbbee26dbc58a5a74
LeonShimazaki <48808986+LeonShimazaki@users.noreply.github.com>
2020-01-13 02:26:38 +1300

    Testing updates

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,25 @@
-fun main(args: Array<String>) {
-	println("Hello world")
+package demo 
+
+import java.util.Random
+
+
+fun main(args : Array<String>){
+    println("Hello, world!")
+    
+    fun add(num1: Int, num2: Int) = num1 + num2
+    println("5 + 4 = ${add(5,4)}")
+
+    val items = listOf("apple", "banana", "kiwifruit", "orange", "kiwifruit")
+    for (item in items) {
+        println(item)
+    }
+    val rand = Random()
+    val magicnumber = rand.nextInt(50) + 1
+
+    var guess = 0
+
+    while(magicnumber != guess){
+        guess += 1
+    }
+    println("Magic number was $guess")
 }
